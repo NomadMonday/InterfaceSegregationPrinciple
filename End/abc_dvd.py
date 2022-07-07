88c4775862270d2fac12af50dfae7941615c2776
NomadMonday <NomadMonday@gmail.com>
2022-07-06 22:10:32 -0700

    Update type hint.

--- End/abc_dvd.py
+++ End/abc_dvd.py
@@ -2,5 +2,5 @@ from abc_library_item import ABCLibraryItem
 
 class ABC_DVD(ABCLibraryItem):
     def __init__(self):
-        self.actors: list = None
+        self.actors: list[str] = None
         self.run_time_in_minutes: int = None
